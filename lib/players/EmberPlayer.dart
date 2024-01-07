0fd91730e11a772727f12986bdb813c3b6dfe96e
carlosgr1992 <carlos.gr1992@gmail.com>
2024-01-08 00:49:42 +0100

    Añadida la clase EmberPlayer

---
+++ lib/players/EmberPlayer.dart
@@ -0,0 +1,22 @@
+import 'package:flame/components.dart';
+import 'package:videojuego_cgr/game/JuegoCarlos.dart';
+
+class EmberPlayer extends SpriteAnimationComponent
+    with HasGameReference<JuegoCarlos> {
+
+  EmberPlayer({
+    required super.position,
+  }) : super(size: Vector2(64,64), anchor: Anchor.center);
+
+  @override
+  void onLoad() {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('ember.png'),
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        textureSize: Vector2.all(16),
+        stepTime: 0.12,
+      ),
+    );
+  }
+}
