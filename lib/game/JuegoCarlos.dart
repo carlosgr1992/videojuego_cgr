af1d296ca84585cbddd7e533d1ce8462f5d69532
carlosgr1992 <carlos.gr1992@gmail.com>
2024-01-08 00:56:34 +0100

    Añadido el emberPlayer al mundo

--- lib/game/JuegoCarlos.dart
+++ lib/game/JuegoCarlos.dart
@@ -2,11 +2,13 @@
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:videojuego_cgr/players/EmberPlayer.dart';
 
 class JuegoCarlos extends FlameGame{
 
   final world = World();
   late final CameraComponent cameraComponent;
+  late EmberPlayer _player1;
 
   @override
   Future<void> onLoad() async {
@@ -21,7 +23,13 @@ class JuegoCarlos extends FlameGame{
     cameraComponent = CameraComponent(world: world);
 
     camera.viewfinder.anchor = Anchor.topLeft;
+
     addAll([cameraComponent, world]);
+
+    _player1 = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_player1);
 
   }
 
