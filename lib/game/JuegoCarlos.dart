a213b9c5f95e93f7add998c580d7d6c357251288
carlosgr1992 <carlos.gr1992@gmail.com>
2024-01-08 18:28:58 +0100

    Añadido el tilemap de forma visual al juego

--- lib/game/JuegoCarlos.dart
+++ lib/game/JuegoCarlos.dart
@@ -2,6 +2,7 @@
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:videojuego_cgr/players/EmberPlayer.dart';
 
 class JuegoCarlos extends FlameGame{
@@ -9,6 +10,7 @@ class JuegoCarlos extends FlameGame{
   final world = World();
   late final CameraComponent cameraComponent;
   late EmberPlayer _player1, _player2;
+  late TiledComponent mapComponent;
 
   @override
   Future<void> onLoad() async {
@@ -26,6 +28,9 @@ class JuegoCarlos extends FlameGame{
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
 
     addAll([cameraComponent, world]);
+
+    mapComponent = await TiledComponent.load('mapa1.tmx', Vector2.all(32));
+    world.add(mapComponent);
 
     _player1 = EmberPlayer(position: Vector2(128, canvasSize.y - 200),);
     _player2 = EmberPlayer(position: Vector2(250, canvasSize.y - 200),);
